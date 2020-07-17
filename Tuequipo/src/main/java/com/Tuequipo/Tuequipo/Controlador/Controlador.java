11cfd83b69c3594535d0d39030954434d9128840
Facu_Barsanti <facubarsanti@gmail.com>
2020-07-17 18:44:25 -0300

    cambios html

--- Tuequipo/src/main/java/com/Tuequipo/Tuequipo/Controlador/Controlador.java
+++ Tuequipo/src/main/java/com/Tuequipo/Tuequipo/Controlador/Controlador.java
@@ -36,7 +36,7 @@ EquipoServicio equipoServicio;
     public String registro(){
         return "Registro.html";
     }
-    
+
     @PostMapping("/registrar")
     public String cargaEquipo(ModelMap modelo,MultipartFile archivo, @RequestParam(required = true) String nombre, @RequestParam String mail, @RequestParam String descripcion, @RequestParam(required = true) String clave1, @RequestParam(required = true) String clave2, @RequestParam String telefono1, @RequestParam String telefono2, @RequestParam Turno turno, @RequestParam Zonas zona, @RequestParam Dias dia, @RequestParam Tipo tipo, @RequestParam Categoria categoria, @RequestParam CantidadJugadores cantidadJugadores){
         try {
@@ -60,7 +60,7 @@ EquipoServicio equipoServicio;
             Logger.getLogger(Controlador.class.getName()).log(Level.SEVERE, null, ex);
             return "Registro.html";
         }
-        return "index.html";
+        return "RegistroExitoso.html";
 }
     
     @GetMapping("/login")
