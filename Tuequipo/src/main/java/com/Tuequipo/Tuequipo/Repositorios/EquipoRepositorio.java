eac4c99adda80f91c08630bb63a516709dcff5cb
FrancoCastro-94 <franco94castro@hotmail.com>
2020-07-17 19:06:10 -0300

    buscador

--- Tuequipo/src/main/java/com/Tuequipo/Tuequipo/Repositorios/EquipoRepositorio.java
+++ Tuequipo/src/main/java/com/Tuequipo/Tuequipo/Repositorios/EquipoRepositorio.java
@@ -1,7 +1,6 @@
 
 package com.Tuequipo.Tuequipo.Repositorios;
 
-import com.Tuequipo.Tuequipo.Enumeracion.Zonas;
 import com.Tuequipo.Tuequipo.entidades.Equipo;
 import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -19,8 +18,19 @@ public interface EquipoRepositorio extends JpaRepository <Equipo,String>{
     @Query ("SELECT c FROM Equipo c WHERE c.mail=:mail")
     public Equipo buscarEquipoPorMail(@Param("mail") String mail);
     
-//    @Query ("SELECT c FROM Equipo c WHERE c.zonas=:zonas")
-//    public List<Equipo> buscarEquipoPorZona(@Param("zonas") Zonas zonas);
+    @Query ("SELECT c FROM Equipo c WHERE c.zona= :zona AND c.disponible= 1")
+    public List<Equipo> buscarEquipoPorZona(@Param("zona") String zona);
+    
+    @Query ("SELECT c FROM Equipo c WHERE c.categoria= :categoria AND c.disponible= 1")
+    public List<Equipo> buscarEquipoPorCategoria(@Param("categoria") String categoria);
+    
+    @Query ("SELECT c FROM Equipo c WHERE c.cantidadJugadores= :cantidadJugadores AND c.disponible= 1")
+    public List<Equipo> buscarEquipoCantidadJugadores(@Param("cantidadJugadores") String cantidadJugadores);
+    
+    @Query ("SELECT c FROM Equipo c WHERE c.turno= :turno AND c.disponible= 1")
+    public List<Equipo> buscarEquipoTurno(@Param("turno") String turno);
 
+    @Query ("SELECT c FROM Equipo c WHERE c.tipo= :tipo AND c.disponible= 1")
+    public List<Equipo> buscarEquipoTipo(@Param("tipo") String tipo);
     
 }
